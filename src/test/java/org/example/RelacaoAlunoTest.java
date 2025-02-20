8ab4ae1f49827f51a68f18cfb3ce4e0f68840de8
ARIEL LORENZ <04529620050@senacrs.edu.br>
2025-02-20 20:42:55 -0300

    ADD:literalmente tudo

--- src/test/java/org/example/RelacaoAlunoTest.java
+++ src/test/java/org/example/RelacaoAlunoTest.java
@@ -2,10 +2,12 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.example.RelacaoAluno.calcularDescanso;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RelacaoAlunoTest {
-    private Aluno aluno1, aluno2, aluno3, aluno4;
+    private Aluno aluno1, aluno2, aluno3, aluno4,aluno5;
 
     @BeforeEach public void setup()
     {
@@ -14,14 +16,23 @@ public class RelacaoAlunoTest {
             aluno2 = new Aluno("Arthur",6911732991041171121113211411110897115.0);
             aluno3 = new Aluno("Natan",34);
             aluno4 = new Aluno("Ney",57);
+            aluno5 = new Aluno("Cristiano ronaldo", 7);
         System.out.println("Novos alunos criados com sucesso!");
     }
 
     @Test public void testDescansoSuficiente()
     {
-        var descanso = RelacaoAluno.calcularDescanso(aluno1);
+        var descanso = calcularDescanso(aluno1);
         System.out.println("Testando se o descando é suficiente para o Aluno1...");
         assertEquals("Descanso diario suficiente:O aluno é um cara tranquilo", descanso);
         System.out.println("O descanso é suficiente para o Aluno1!");
     }
+
+
+    @Test public void testDescansoExausto() {
+        String descanso = calcularDescanso(aluno4);
+        assertEquals("Alerta! O aluno está completamente exausto!", descanso);
+    }
+
 }
+
